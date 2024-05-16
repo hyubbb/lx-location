23fcbcf18150303885fc122f9d7c2abee439e2ed
hyubbb <dlckdguq1011@gmail.com>
2024-05-16 23:35:07 +0900

    first commit

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Map from "./Map";
+
+const HomePage = () => {
+  return (
+    <div className='App'>
+      <h1>Kakao 지도 예제</h1>
+
+      <Map />
+    </div>
+  );
+};
+
+export default HomePage;
